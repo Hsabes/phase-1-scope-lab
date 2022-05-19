72049cc1b8d89a694066965476bbaed4c5f94a6a
Harrison Sabean <hsabes@gmail.com>
2022-05-19 10:21:09 -0700

    commit tree bf11272b394ef44a563318e1ac671943f0fe3ba9

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+var bestCustomer = undefined;
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = undefined;
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'cannot be change bro!'
+}
+
+// console.log(upperCaseCustomerName(customerName));
